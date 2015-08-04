0e0ff1052a60f8e63e4b30b0bad9d7d084754d63
Victor <Victor@iMac-Nest.local>
2015-08-04 18:51:49 +0300

    Add ffmpeg libs

--- RtspStreamer/ViewController.m
+++ RtspStreamer/ViewController.m
@@ -7,21 +7,23 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "RTSPPlayer.h"
 
 @implementation ViewController
+{
+    RTSPPlayer *player;
+}
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    player = [[RTSPPlayer alloc] initWithRtspAudioUrl:@"rtsp://192.168.1.30:1935/live/myStream"];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [player play];
+        NSLog(@"Session closed");
+    });
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
